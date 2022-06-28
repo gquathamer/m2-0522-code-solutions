5e83e4e584b0d56e5e932a5669ea69ddf295c5d3
Garrett Quathamer <gquathamer@gmail.com>
2022-06-27 21:42:27 -0700

    Revert "completed node-fs-writefile"

--- node-fs-writefile/note-to-self.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const userInput = process.argv[2];
-
-fs.writeFile('note.txt', userInput + '\n', 'utf-8', error => {
-  if (error) throw error;
-});
--- node-fs-writefile/write-rand.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const randomNumber = Math.random().toString();
-
-fs.writeFile('random.txt', randomNumber + '\n', 'utf-8', error => {
-  if (error) throw error;
-});
