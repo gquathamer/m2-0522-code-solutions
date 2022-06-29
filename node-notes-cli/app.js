9eedfadddd2bb93c34f4cfca30237b5bb4b00445
Garrett Quathamer <gquathamer@gmail.com>
2022-06-29 03:55:07 +0000

    can add new note

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,9 +1,21 @@
 const fs = require('fs');
+const jsonData = require('./data.json');
+const operation = process.argv[2];
 
-fs.readFile('data.json', 'utf-8', (err, data) => {
-  if (err) throw err;
-  const jsonData = JSON.parse(data);
-  for (const note in jsonData.notes) {
-    console.log(`${note}: ${jsonData.notes[note]}`);
-  }
-});
+if (operation === 'read') {
+  fs.readFile('data.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+    for (const note in jsonData.notes) {
+      console.log(`${note}: ${jsonData.notes[note]}`);
+    }
+  });
+} else if (operation === 'create') {
+  const newNote = process.argv[3];
+  jsonData.notes[jsonData.nextId] = newNote;
+  jsonData.nextId++;
+  const newJsonData = JSON.stringify(jsonData, null, 2);
+  fs.writeFile('data.json', newJsonData, err => {
+    if (err) throw err;
+    console.log(newJsonData);
+  });
+}
