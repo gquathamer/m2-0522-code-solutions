3d2955a60d46551c844874787fdc7330786624ff
Garrett Quathamer <gquathamer@gmail.com>
2022-06-29 01:57:29 +0000

    adjusted the syntax of if statement to create new pull request. merged previous

--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -3,5 +3,7 @@ const fs = require('fs');
 const randomNumber = Math.random().toString();
 
 fs.writeFile('random.txt', randomNumber + '\n', 'utf-8', error => {
-  if (error) throw error;
+  if (error) {
+    throw error;
+  }
 });
