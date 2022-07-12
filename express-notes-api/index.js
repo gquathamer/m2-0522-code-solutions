3bebcb5c759771e7aa423c8d9380ead79c492589
Garrett Quathamer <gquathamer@gmail.com>
2022-07-12 02:45:08 +0000

    user can get a single note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -9,3 +9,13 @@ app.get('/api/notes', (req, res) => {
   }
   res.status(200).json(notesArray);
 });
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  if (parseInt(id) < 0 || isNaN(id)) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (notesObject.notes[id]) {
+    res.status(200).json(notesObject.notes[id]);
+  } else if (!notesObject.notes[id]) {
+    res.status(404).json({ error: `cannot find note with id ${id}` });
+  }
+});
