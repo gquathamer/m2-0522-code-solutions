270aca67066a87fc734c6af32fa4123072ebedfa
Garrett Quathamer <gquathamer@gmail.com>
2022-07-12 04:15:16 +0000

    users can post a new note

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const notesObject = require('./data.json');
+const fs = require('fs');
 const app = express();
 app.listen(3000);
+const jsonParser = express.json();
+app.use(jsonParser);
+
 app.get('/api/notes', (req, res) => {
   const notesArray = [];
   for (const note in notesObject.notes) {
@@ -9,6 +13,7 @@ app.get('/api/notes', (req, res) => {
   }
   res.status(200).json(notesArray);
 });
+
 app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id;
   if (parseInt(id) < 0 || isNaN(id)) {
@@ -17,5 +22,23 @@ app.get('/api/notes/:id', (req, res) => {
     res.status(200).json(notesObject.notes[id]);
   } else if (!notesObject.notes[id]) {
     res.status(404).json({ error: `cannot find note with id ${id}` });
+  }
+});
+
+app.post('/api/notes', (req, res) => {
+  if (req.body.content === undefined) {
+    res.status(400).json({ error: 'content is a required field' });
+  } else if (req.body.content !== undefined) {
+    notesObject.notes[notesObject.nextId] = req.body;
+    notesObject.notes[notesObject.nextId].id = notesObject.nextId;
+    notesObject.nextId++;
+    const stringifiedNotesObject = JSON.stringify(notesObject);
+    fs.writeFile('data.json', stringifiedNotesObject, 'utf-8', err => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occurred' });
+      }
+      res.status(201).json(notesObject.notes[notesObject.nextId - 1]);
+    });
   }
 });
