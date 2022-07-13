a9cc199b05ca3508dc11197c6eb8e0805e79a05c
Garrett Quathamer <gquathamer@gmail.com>
2022-07-13 04:11:11 +0000

    made necessary changes

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const notesObject = require('./data.json');
 const fs = require('fs');
 const app = express();
-app.listen(3000);
-const jsonParser = express.json();
-app.use(jsonParser);
+app.use(express.json());
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('listening on port 3000');
+});
 
 app.get('/api/notes', (req, res) => {
   const notesArray = [];
@@ -16,11 +18,11 @@ app.get('/api/notes', (req, res) => {
 
 app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id;
-  if (parseInt(id) < 0 || isNaN(id)) {
+  if (parseInt(id) <= 0 || isNaN(id)) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (notesObject.notes[id]) {
     res.status(200).json(notesObject.notes[id]);
-  } else if (!notesObject.notes[id]) {
+  } else {
     res.status(404).json({ error: `cannot find note with id ${id}` });
   }
 });
@@ -33,19 +35,20 @@ app.post('/api/notes', (req, res) => {
     notesObject.notes[notesObject.nextId].id = notesObject.nextId;
     notesObject.nextId++;
     const stringifiedNotesObject = JSON.stringify(notesObject);
-    fs.writeFile('data.json', stringifiedNotesObject, 'utf-8', err => {
+    fs.writeFile('derp/data.json', stringifiedNotesObject, 'utf-8', err => {
       if (err) {
         console.error(err);
         res.status(500).json({ error: 'An unexpected error occurred' });
+      } else {
+        res.status(201).json(notesObject.notes[notesObject.nextId - 1]);
       }
-      res.status(201).json(notesObject.notes[notesObject.nextId - 1]);
     });
   }
 });
 
 app.delete('/api/notes/:id', (req, res) => {
   const id = req.params.id;
-  if (parseInt(id) < 0 || isNaN(id)) {
+  if (parseInt(id) <= 0 || isNaN(id)) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (!notesObject.notes[id]) {
     res.status(404).json({ error: `Cannot find note with id ${id}` });
@@ -56,15 +59,16 @@ app.delete('/api/notes/:id', (req, res) => {
       if (err) {
         console.error(err);
         res.status(500).json({ error: 'An unexpected error occurred' });
+      } else {
+        res.status(204).send();
       }
-      res.status(204).send();
     });
   }
 });
 
-app.post('/api/notes/:id', (req, res) => {
+app.put('/api/notes/:id', (req, res) => {
   const id = req.params.id;
-  if (parseInt(id) < 0 || isNaN(id)) {
+  if (parseInt(id) <= 0 || isNaN(id)) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (req.body.content === undefined) {
     res.status(400).json({ error: 'content is a required field' });
@@ -79,8 +83,9 @@ app.post('/api/notes/:id', (req, res) => {
           if (err) {
             console.error(err);
             res.status(500).json({ error: 'An unexpected error has occurred' });
+          } else {
+            res.status(200).json(notesObject.notes[note]);
           }
-          res.status(200).json(notesObject.notes[note]);
         });
       }
     }
