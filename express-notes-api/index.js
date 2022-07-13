03911d7309b16367db79b524c1feac76480c00f3
Garrett Quathamer <gquathamer@gmail.com>
2022-07-13 02:47:20 +0000

    completed express-notes-api

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -42,3 +42,47 @@ app.post('/api/notes', (req, res) => {
     });
   }
 });
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  if (parseInt(id) < 0 || isNaN(id)) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (!notesObject.notes[id]) {
+    res.status(404).json({ error: `Cannot find note with id ${id}` });
+  } else if (notesObject.notes[id]) {
+    delete notesObject.notes[id];
+    const stringifiedNotesObject = JSON.stringify(notesObject);
+    fs.writeFile('data.json', stringifiedNotesObject, 'utf-8', err => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occurred' });
+      }
+      res.status(204).send();
+    });
+  }
+});
+
+app.post('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  if (parseInt(id) < 0 || isNaN(id)) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (req.body.content === undefined) {
+    res.status(400).json({ error: 'content is a required field' });
+  } else if (!notesObject.notes[id]) {
+    res.status(404).json({ error: `Cannot find note with id ${id}` });
+  } else if (req.body.content !== undefined) {
+    for (const note in notesObject.notes) {
+      if (parseInt(id) === notesObject.notes[note].id) {
+        notesObject.notes[note].content = req.body.content;
+        const stringifiedNotesObject = JSON.stringify(notesObject);
+        fs.writeFile('data.json', stringifiedNotesObject, 'utf-8', err => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An unexpected error has occurred' });
+          }
+          res.status(200).json(notesObject.notes[note]);
+        });
+      }
+    }
+  }
+});
