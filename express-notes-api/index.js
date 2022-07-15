b762680dfbdc988a7483e1190ad7610e7914d672
Garrett Quathamer <gquathamer@gmail.com>
2022-07-15 02:08:50 +0000

    removed for in loop and if conditional checking if a note object matched a given id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -71,8 +71,8 @@ app.delete('/api/notes/:id', (req, res) => {
 });
 
 app.put('/api/notes/:id', (req, res) => {
-  const id = req.params.id;
-  if (parseInt(id) <= 0 || isNaN(id)) {
+  const id = parseInt(req.params.id);
+  if (id <= 0 || isNaN(id)) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (!Number.isInteger(id)) {
     res.status(400).json({ error: 'id must be a positive integer' });
@@ -80,20 +80,16 @@ app.put('/api/notes/:id', (req, res) => {
     res.status(400).json({ error: 'content is a required field' });
   } else if (!notesObject.notes[id]) {
     res.status(404).json({ error: `Cannot find note with id ${id}` });
-  } else if (req.body.content !== undefined) {
-    for (const note in notesObject.notes) {
-      if (parseInt(id) === notesObject.notes[note].id) {
-        notesObject.notes[note].content = req.body.content;
-        const stringifiedNotesObject = JSON.stringify(notesObject);
-        fs.writeFile('data.json', stringifiedNotesObject, 'utf-8', err => {
-          if (err) {
-            console.error(err);
-            res.status(500).json({ error: 'An unexpected error has occurred' });
-          } else {
-            res.status(200).json(notesObject.notes[note]);
-          }
-        });
+  } else {
+    notesObject.notes[id].content = req.body.content;
+    const stringifiedNotesObject = JSON.stringify(notesObject);
+    fs.writeFile('data.json', stringifiedNotesObject, 'utf-8', err => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error has occurred' });
+      } else {
+        res.status(200).json(notesObject.notes[id]);
       }
-    }
+    });
   }
 });
