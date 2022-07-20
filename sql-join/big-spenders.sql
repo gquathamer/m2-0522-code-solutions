2c1aa38ebdf6780d713ed40272e124f2c996bbe3
Garrett Quathamer <gquathamer@gmail.com>
2022-07-20 04:27:00 +0000

    changed big-spender to show only required field names

--- sql-join/big-spenders.sql
+++ sql-join/big-spenders.sql
@@ -1,4 +1,6 @@
-select *
+select "payments"."amount",
+  "customers"."firstName",
+  "customers"."lastName"
 from "payments"
 join "customers" using ("customerId")
 order by "payments"."amount" DESC
