bc1c7a1149b0f72c57f298998ec63d7e80de9f7d
Garrett Quathamer <gquathamer@gmail.com>
2022-07-25 04:25:16 +0000

    completed es6-promises

---
+++ es6-promises/index.js
@@ -0,0 +1,8 @@
+const takeAChance = require('./take-a-chance');
+const returnValue = takeAChance('Garrett');
+returnValue.then(value => {
+  console.log(value);
+});
+returnValue.catch(error => {
+  console.error(error);
+});
